ed5742a2a4e3bedad3aa125ba4b0d24ee7a571b0
Melf Stöcken <melf@stillleben.media>
2018-02-21 18:16:32 +0100

    adden functionality for table still TODOs left

--- AroioRM/MainMenu/Convolver/ConvolverModulViewController.swift
+++ AroioRM/MainMenu/Convolver/ConvolverModulViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 import McPicker
 
-class ConvolverView: UIView, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, BankTableViewDelegate {
-    
+class ConvolverView: UIView, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+
     //MAKR: Properties
     
     //Variable for active Filter
     var defCoeff: String = ""
     var defCoeffInt: Int  = 0
     
-    var tableCells: [UITableViewCell] = []
-    
-    let bankTableViewCell: BankTableViewCell? = nil
+    var filterPickerData: [[String]] = [[]]
     
     @IBOutlet weak var convolutionSwitch: UISwitch!
     @IBOutlet weak var preFilterSwitch: UISwitch!
@@ -39,8 +37,6 @@ class ConvolverView: UIView, UITableViewDataSource, UITableViewDelegate, UITextF
         bankTableView.dataSource = self
         bankTableView.delegate = self
         
-        bankTableViewCell?.delegate = self
-        
         let nibName = UINib(nibName: "BankTableViewCell", bundle: nil)
         bankTableView.register(nibName, forCellReuseIdentifier: "BankTableViewCell")
         
@@ -50,6 +46,8 @@ class ConvolverView: UIView, UITableViewDataSource, UITableViewDelegate, UITextF
         if defCoeff != "" {
             defCoeffInt = Int(String(defCoeff.filter { !" \n".contains($0) }))!
         }
+     
+        addFilterPickerData()
     }
     
     override init(frame: CGRect) {
@@ -82,25 +80,7 @@ class ConvolverView: UIView, UITableViewDataSource, UITableViewDelegate, UITextF
     }
     @IBAction func saveAndReloadPressed(_ sender: UIButton) {
     }
-    
-    //MARK: Delegate Methods
-    
-    func bankSwitchTapped(at index: IndexPath) {
-        
-    }
-    
-    func bankNoteTextFieldTapped(textField: UITextField) {
-        AroioObject.aroio?.sendRequestToSocket(request: "PLAYERNAME", newValue: textField.text!)
-    }
-    
-    func bankFilterSelctionButtonTapped(_ button: UIButton?) {
-        AroioObject.aroio?.sendRequestToSocket(request: "TEST", newValue: button!.currentTitle!)
-    }
-    
-    func bankVolumeTextFieldTapped(at index: IndexPath, textField: UITextField) {
-        
-    }
-    
+
     // MARK: - Table view data source
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -130,37 +110,93 @@ class ConvolverView: UIView, UITableViewDataSource, UITableViewDelegate, UITextF
             cell.bankSwitch.isOn = true
         }
         
-        cell.bankNoteTextField.text = (AroioObject.aroio?.getUserconfigParameter(request: "COEFF_NAME\(indexPath.row)"))!
-        cell.bankNoteTextField.delegate = self
-        
-        
-        cell.bankFilterSelectionButton.setTitle((AroioObject.aroio?.getUserconfigParameter(request: "COEFF_COMMENT\(indexPath.row)"))!, for: .normal)
-        
+        cell.bankNoteTextField.text = (AroioObject.aroio?.getUserconfigParameter(request: "COEFF_COMMENT\(indexPath.row)"))!
+        cell.bankFilterSelectionButton.setTitle((AroioObject.aroio?.getUserconfigParameter(request: "COEFF_NAME\(indexPath.row)"))!, for: .normal)
         cell.bankVolumeTextField.text = "-\((AroioObject.aroio?.getUserconfigParameter(request: "COEFF_ATT\(indexPath.row)"))!)"
-        cell.bankVolumeTextField.delegate = self
         
+        // change functions für cell fields
+        cell.bankSwitch.tag = indexPath.row
+        cell.bankSwitch.addTarget(self, action: #selector(ConvolverView.bankSwitchChanged), for: .valueChanged)
+        
+        cell.bankNoteTextField.tag = indexPath.row
+        cell.bankNoteTextField.addTarget(self, action: #selector(ConvolverView.bankNoteTextFieldEditingDidEnd), for: .editingDidEnd)
+        
+        cell.bankFilterSelectionButton.tag = indexPath.row
         cell.bankFilterSelectionButton.addTarget(self, action: #selector(ConvolverView.bankFilterSelectionButtonPressed), for: .touchUpInside)
         
+        cell.bankVolumeTextField.tag = indexPath.row
+        cell.bankVolumeTextField.addTarget(self, action: #selector(ConvolverView.bankVolumeTextFieldEditingDidEnd), for: .editingDidEnd)
         
-        tableCells.append(cell)
+        
         return cell
     }
     
-    @objc func bankFilterSelectionButtonPressed() {
-        print("bankFilterSelectionButtonPressed() was called")
+    func changeSwitchesInTableView(cellFromTable: UITableViewCell){
+        //TODO: Not working for now
+        
+        for cell in self.bankTableView.visibleCells {
+            
+            if cell == cellFromTable  {
+                
+                bankTableView.indexPath(for: cellFromTable)
+            } else {
+                
+            }
+        }
     }
     
-    func createTableFunctions(cell: BankTableViewCell) {
+    @objc func bankSwitchChanged(sender: UISwitch, cell: UITableViewCell) {
+        //TODO: Make switches that only one is active
+        AroioObject.aroio?.sendRequestToSocket(request: "DEF_COEFF", newValue: String(sender.tag))
         
-        //        bankTableView.cellForRow(at: inde)
+        changeSwitchesInTableView(cellFromTable: cell)
         
-        //TODO: Make Cells writeable
+        for index in 0...9 {
+            if index == sender.tag  {
+                sender.isOn = true
+            } else {
+                
+            }
+        }
+        
+        if self.convolutionSwitch.isOn == true {
+            self.bankTableView.reloadData()
+            AroioObject.aroio?.sendRequestToSocket(request: "DEF_COEFF\(sender.tag)", newValue: "ON")
+        } else {
+            self.bankTableView.reloadData()
+            AroioObject.aroio?.sendRequestToSocket(request: "BRUTEFIR", newValue: "OFF")
+        }
+        
+        AroioObject.aroio?.sendRequestToSocket(request: "COEFF_NAME\(sender.tag)", newValue: "ON")
+    }
+    @objc func bankNoteTextFieldEditingDidEnd(sender: UITextField) {
+        AroioObject.aroio?.sendRequestToSocket(request: "COEFF_COMMENT\(sender.tag)", newValue: sender.text!)
+    }
+    @objc func bankFilterSelectionButtonPressed(sender: UIButton) {
+        //TODO: Put picker into fields with array
+        AroioObject.aroio?.sendRequestToSocket(request: "COEFF_NAME\(sender.tag)", newValue: "TEST")
+     
+        let filterPicker = McPicker(data: self.filterPickerData)
+        filterPicker.toolbarButtonsColor = .white
+        filterPicker.toolbarBarTintColor = UIColor(red:0.00, green:0.50, blue:0.50, alpha:1.0)
+
+        filterPicker.show {  (selections: [Int : String]) -> Void in
+            if let name = selections[0] {
+                sender.setTitle(name, for: .normal)
+                AroioObject.aroio?.sendRequestToSocket(request: "VOLUME", newValue: name)
+            }
+        }
         
     }
+    @objc func bankVolumeTextFieldEditingDidEnd(sender: UITextField) {
+        AroioObject.aroio?.sendRequestToSocket(request: "COEFF_ATT\(sender.tag)", newValue: sender.text!)
+    }
     
-    
-    
-    
+    func addFilterPickerData(){
+        for index in 0...9 {
+            self.filterPickerData[0].append((AroioObject.aroio?.getUserconfigParameter(request: "COEFF_NAME\(index)"))!)
+        }
+    }
     
 }
 
