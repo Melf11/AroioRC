0c0051a7e1fb59019cd9cbee1257abdcff4b8b30
Melf Stöcken <melf@stillleben.media>
2018-02-21 18:15:41 +0100

    deleted delegates - no need anymore

--- AroioRM/MainMenu/Convolver/BankTableViewCell.swift
+++ AroioRM/MainMenu/Convolver/BankTableViewCell.swift
@@ -8,42 +8,15 @@
 
 import UIKit
 
-protocol BankTableViewDelegate {
-    func bankSwitchTapped(at index:IndexPath)
-    func bankNoteTextFieldTapped(textField: UITextField)
-    func bankFilterSelctionButtonTapped(_ button: UIButton?)
-    func bankVolumeTextFieldTapped(at index:IndexPath, textField: UITextField)
-}
-
 class BankTableViewCell: UITableViewCell {
+  //MARK: Properties
 
-    //TODO: Find a way to send change requests from every cell separetly
-    
-    //MARK: Properties
-    
-    var delegate: BankTableViewDelegate?
-    
     @IBOutlet weak var bankSwitch: UISwitch!
     @IBOutlet weak var bankNoteTextField: UITextField!
     @IBOutlet weak var bankFilterSelectionButton: UIButton!
     @IBOutlet weak var bankVolumeTextField: UITextField!
 
     var indexPath:IndexPath!
-
-    @IBAction func bankSwitchPressed(_ sender: UISwitch) {
-        delegate?.bankSwitchTapped(at: indexPath)
-    }
-    @IBAction func bankNoteTextFieldUsed(_ sender: UITextField) {
-        delegate?.bankNoteTextFieldTapped(textField: sender)
-    }
-    @IBAction func bankFilterSelectionButton(_ sender: UIButton) {
-        delegate?.bankFilterSelctionButtonTapped(sender)
-    }
-    @IBAction func bankVolumeTextFieldUsed(_ sender: UITextField) {
-        delegate?.bankVolumeTextFieldTapped(at: indexPath, textField: sender)
-    }
-
-
     
     override func awakeFromNib() {
         super.awakeFromNib()
